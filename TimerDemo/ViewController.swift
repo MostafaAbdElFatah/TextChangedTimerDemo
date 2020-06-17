76aec9f0e1d246391b2bf9928743f5d1fa7bac7d
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2020-06-17 17:41:32 +0200

    Initial Commit

---
+++ TimerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerDemo
+//
+//  Created by Mostafa Abd ElFatah on 6/17/20.
+//  Copyright © 2020 asd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
